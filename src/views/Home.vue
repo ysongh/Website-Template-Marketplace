ef1290a04d7aab1679b94d3331db026c1d81c8c2
You Song Hou <yousonghou@gmail.com>
2022-08-05 20:04:59 -0400

    feat: add a card template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,89 @@
 <template>
-  <div class="home">
-    <v-btn
-      color="orange"
-      elevation="2"
-      @click="goToFormPage()"
-    >
-      Post Contract
-    </v-btn>
-  </div>
+  <v-container>
+    <div class="d-flex justify-space-between align-center mb-4">
+      <h1>List of Website Templates</h1>
+      <v-btn
+        class="mb-3 float-left"
+        color="orange"
+        elevation="2"
+        @click="goToFormPage()"
+      >
+        Add Template
+      </v-btn>
+    </div>
+    
+    <v-row>
+      <v-col
+        sm="12"
+        md="4"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          <v-img
+            height="250"
+            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+          ></v-img>
+          <v-card-actions>
+            <v-btn
+              color="deep-purple lighten-2"
+              block
+            >
+              Reserve
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col
+        sm="12"
+        md="4"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          <v-img
+            height="250"
+            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+          ></v-img>
+          <v-card-actions>
+            <v-btn
+              color="deep-purple lighten-2"
+              block
+            >
+              Reserve
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col
+        sm="12"
+        md="4"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          <v-img
+            height="250"
+            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+          ></v-img>
+          <v-card-actions>
+            <v-btn
+              color="deep-purple lighten-2"
+              block
+            >
+              Reserve
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
